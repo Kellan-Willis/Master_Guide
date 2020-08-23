08b8094f28cd1fe5450f01be12112f53933c18b6
Kellan-Willis <69184612+Kellan-Willis@users.noreply.github.com>
2020-08-22 23:18:39 -1000

    Add files via upload

---
+++ output_Design1.java
@@ -0,0 +1,33 @@
+/*
+
+4.  output_design
+
+Write a program that displays the following 
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *  
+
+*/
+
+public class output_Design1{
+	public static void main (String [] args){
+		//variables
+		//input
+		//processing
+		//output
+		System.out.println ( "   *   ");
+		System.out.println ( "  ***  ");
+		System.out.println ( " ***** ");
+		System.out.println ( "*******");
+		System.out.println ( " ***** ");
+		System.out.println ( "  ***  ");
+		System.out.println ( "   *   ");
+		
+		
+		
+	}
+}
